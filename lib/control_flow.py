aba11c9d08d374a34c8a86654c1da16332cf7c01
Maganga Mwambonu <codebymaganga@gmail.com>
2023-11-27 21:50:12 +0300

    Assignment Completed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    saved_name = username.upper()
+    if saved_name == 'ADMIN' and password == str(12345):
+        print('Access Granted')
+    else:
+        print('Access Denied')
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+   if temperature < 40 :
+       print("It's brisk out there!")
+   elif temperature > 40 and temperature < 65 :
+        print("It's a little chilly out there!")
+   elif temperature > 85 : 
+       print("It's too dang hot out there!")
+   else:
+        print("It's perfect out there!")
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(num)
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '/':
+        return num1 / num2
+    elif operation == '*':
+        return num1 * num2
+    else:
+        print( "Invalid operation!" )
+        return None
+
+
+
+calculator("+", 1, 1)
